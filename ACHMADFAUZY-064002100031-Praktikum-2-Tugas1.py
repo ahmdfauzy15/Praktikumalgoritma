8ca2c142bb3fcc96f98273287b6140d3d6bb6372
ahmdfauzy15 <91053494+ahmdfauzy15@users.noreply.github.com>
2021-09-27 13:51:20 +0700

    tugas 1 Achmad fauzy

---
+++ ACHMADFAUZY-064002100031-Praktikum-2-Tugas1.py
@@ -0,0 +1,24 @@
+import math
+a = float(input("Masukan nilai a : "))
+b = float(input("Masukan nilai b : "))
+
+total = a + b
+print("a ditambah b adalah",total)
+
+total = a - b
+print("Selisih antara a dan b adalah",abs(total))
+
+total = a * b
+print("Jumlah a dikali b adalah",total)
+
+total = a % b
+print("Jumlah sisa pembagian a dan b",total)
+
+total = a / b
+print("Jumlah pembagian a dan b",total)
+
+total = math.log10(a)
+print("Jumlah dari log a",total)
+
+hasil = a ** b
+total("Jumlah dari a pangkat b",total)
